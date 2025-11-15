964d58114736c8e2916d148fd0edd7848632cf7f
y8pcym7syy-svg <y8pcym7syy@privaterelay.appleid.com>
2025-11-14 20:22:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function addToCart() {
+    alert("Item added to cart!");
+}
+
+function viewCart() {
+    alert("Viewing cart... (cart is currently empty)");
+}
+
+function subscribe() {
+    alert("Congrats! You subscribed!");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Your form has been submitted!");
+}
